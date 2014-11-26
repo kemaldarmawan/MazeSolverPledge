93610f4f9d0437d0c8d57c99b32172e8548a3266
Kemal Darmawan <kemal30darmawan@gmail.com>
2014-11-26 14:13:21 +0700

    First Commit

---
+++ Config.java
@@ -0,0 +1,12 @@
+
+public class Config {
+	public static int counter = 0;
+	
+	public static final float WHEELDIAMETER = 1.25f;
+	public static final float TRACKWIDTH = 3.4f;
+	
+	public static final int BLOCK = 50;
+	public static final int TURNRATE = 100;
+	public static final int DISTANCE = 10;
+	
+}
---
+++ MazeSolverPledge.java
@@ -0,0 +1,17 @@
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+
+public class MazeSolverPledge {
+
+	public static void main(String[] args) {
+		Config.counter = 0;
+		
+		Behavior moveForward = new Passive();
+		Behavior turnLeft = new Active();
+		
+		Behavior []bList = {moveForward, turnLeft};
+		Arbitrator arby = new Arbitrator(bList);
+		arby.start();
+	}
+
+}
